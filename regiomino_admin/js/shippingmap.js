974fea19824b295538ab69f9007b2df4ea63eee1
root <root@ubuntu.(none)>
2014-06-30 18:00:31 +0200

    Neue KML Files hinzugefuegt und Seite unter Regionalbetreuung optimiert

--- regiomino_admin/js/shippingmap.js
+++ regiomino_admin/js/shippingmap.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function($) {
 					var myOptions = {
 							zoom: 9,
 							center: results[0].geometry.location,
-							//center: new google.maps.LatLng(41.875696,-87.624207),
+							//center: new google.maps.LatLng(24.886436490787712, -70.2685546875),
 							mapTypeId: google.maps.MapTypeId.ROADMAP
 					}
 					map = new google.maps.Map(document.getElementById("map"), myOptions);
@@ -19,11 +19,79 @@ jQuery(document).ready(function($) {
 							map: map,
 							position: results[0].geometry.location
 					});
-					var zipCodeLayer = new google.maps.KmlLayer({
-						url: Drupal.settings.basePath + 'sites/all/modules/regiomino_admin/kml/9.kml'
-						//url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
+					
+					data = new Object,
+					callback_url = Drupal.settings.basePath + 'regiomino-geolocation-coordinatestring';
+					data['metro_key'] = Drupal.settings.SHIPPING_OPTION_METRO_KEY;
+					data['field'] = 'field_pickup_areas';
+					
+					$.ajax({
+							url: callback_url,
+							type: 'POST',
+							data: data,
+							success: function (data, textStatus, jqXHR) {
+								var polygons = data.split('|');
+								for(var i=1; i<polygons.length; i++) {
+									var polygon = polygons[i].split(' ');
+									var polygonCoords = new Array();
+									for(var j=0; j<polygon.length; j++) {
+										var polygonpoint = polygon[j].split(',');
+										polygonCoords.push(new google.maps.LatLng(polygonpoint[1], polygonpoint[0]));
+										console.log(polygonpoint[1], polygonpoint[0]);
+									}
+									var zipcodePolygon = new google.maps.Polygon({
+										paths: polygonCoords,
+										strokeColor: '#FF0000',
+										strokeOpacity: 0.8,
+										strokeWeight: 2,
+										fillColor: '#FF0000',
+										fillOpacity: 0.35
+									});
+									zipcodePolygon.setMap(map);
+								}
+							},
+							error: function (http) {
+							},
+							complete: function() {	
+							}
 					});
-					zipCodeLayer.setMap(map);
+					
+					data = new Object,
+					callback_url = Drupal.settings.basePath + 'regiomino-geolocation-coordinatestring';
+					data['metro_key'] = Drupal.settings.SHIPPING_OPTION_METRO_KEY;
+					data['field'] = 'field_delivery_areas';
+					
+					$.ajax({
+							url: callback_url,
+							type: 'POST',
+							data: data,
+							success: function (data, textStatus, jqXHR) {
+								var polygons = data.split('|');
+								for(var i=1; i<polygons.length; i++) {
+									var polygon = polygons[i].split(' ');
+									var polygonCoords = new Array();
+									for(var j=0; j<polygon.length; j++) {
+										var polygonpoint = polygon[j].split(',');
+										polygonCoords.push(new google.maps.LatLng(polygonpoint[1], polygonpoint[0]));
+										console.log(polygonpoint[1], polygonpoint[0]);
+									}
+									var zipcodePolygon = new google.maps.Polygon({
+										paths: polygonCoords,
+										strokeColor: '#00FF00',
+										strokeOpacity: 0.8,
+										strokeWeight: 2,
+										fillColor: '#00FF00',
+										fillOpacity: 0.35
+									});
+									zipcodePolygon.setMap(map);
+								}
+							},
+							error: function (http) {
+							},
+							complete: function() {	
+							}
+					});				
+					
 			}
 	});
 	
